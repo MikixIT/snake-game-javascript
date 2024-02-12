1427a4788a28e274605147575cc55c1235c34a18
MikixIT <michael.46@live.it>
2024-02-12 23:02:45 +0100

    complete project, adding dynamic background with css animation, adding button on gameover for restart game and fix some style and seconds for animations

--- game.js
+++ game.js
@@ -1,13 +1,13 @@
-const canvas = document.querySelector('#game');
-const ctx = canvas.getContext('2d');
-const scoreHTML = document.querySelector("#score")
+const canvas = document.querySelector("#game");
+const ctx = canvas.getContext("2d");
+const scoreHTML = document.querySelector("#score");
 
- class SnakePart{
-     constructor (x ,y){
-        this.x = x;
-        this.y = y;
-     }
- }
+class SnakePart {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+}
 
 /* 
  Obiettivo 🎯:
@@ -25,13 +25,37 @@ const scoreHTML = document.querySelector("#score")
 
 */
 
+let leaderboard = [
+  { name: "Paperino", score: 30 },
+  { name: "Vegeta", score: 27 },
+  { name: "Pasqualino", score: 15 },
+  { name: "Gianperino Prappazzoni", score: 7 },
+];
+
+const updateLeaderboard = (name, score) => {
+  leaderboard.push({ name, score });
+  leaderboard.sort((a, b) => b.score - a.score);
+  // Limita la classifica alle prime 10 posizioni
+  leaderboard.splice(10);
+};
+
+const showLeaderboard = () => {
+  const leaderboardElement = document.querySelector("#leaderboard");
+  leaderboardElement.innerHTML = "";
+
+  leaderboard.forEach((player, i) => {
+    const position = i + 1;
+    leaderboardElement.innerHTML += `<p>${position}. ${player.name} - ${player.score}</p>`;
+  });
+};
+
 const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
 
 let speed = 9;
 let gameTimeout = null;
 
 let tileCount = 20;
-let tileSize = canvas.width / tileCount -2;
+let tileSize = canvas.width / tileCount - 2;
 
 //snake
 let headX = 10;
@@ -54,107 +78,120 @@ let score = 0;
 
 //game loop
 let drawGame = () => {
-     changeSnakePosition();
-     let result = isGameover();
-     if(result){
-        return;
-     }
-     clearScreen();
-     drawSnake();
-     drawFruit();
-     drawScore();
-     console.log(speed)
-     
-     checkFruitCollision();
+  changeSnakePosition();
+  let result = isGameover();
+  if (result) {
+    return;
+  }
+  clearScreen();
+  drawSnake();
+  drawFruit();
+  drawScore();
+  updateLeaderboard();
 
-    if (gameTimeout === null) {
-        gameTimeout = setTimeout(() => {
-          gameTimeout = null;
-          drawGame();
-        }, 1000 / speed);
-    }
-}
+  checkFruitCollision();
+
+  if (gameTimeout === null) {
+    gameTimeout = setTimeout(() => {
+      gameTimeout = null;
+      drawGame();
+    }, 1000 / speed);
+  }
+};
 
 let isGameover = () => {
-    let gameOver = false;
+  let gameOver = false;
 
-    if( yVelocity === 0 && xVelocity === 0){
-        return false;
+  if (yVelocity === 0 && xVelocity === 0) {
+    return false;
+  }
+
+  // walls
+  if (headX < 0 || headY < 0 || headX >= tileCount || headY >= tileCount) {
+    gameOver = true;
+  }
+
+  //collision with himself
+  for (let i = 0; i < snakeParts.length; i++) {
+    let part = snakeParts[i];
+    if (part.x === headX && part.y === headY) {
+      gameOver = true;
+      break;
     }
+  }
 
-    // walls
-    if (headX < 0 || headY < 0 || headX >= tileCount || headY >= tileCount) {
-        gameOver = true;
-    }
+  if (gameOver) {
+    document.body.innerHTML = `
+      <h1>Game over 💀</h1>
+      <h1 id="score">Hai totalizzato: ${score} Points</h1>
+      <p id="score">🐍</p>
+      <button class="restart-button">Riprova</button>
+    `;
+    const restartButton = document.querySelector(".restart-button");
+    restartButton.addEventListener("click", () => {
+      document
+        .querySelector(".restart-button")
+        .addEventListener("click", function () {
+          window.location.reload();
+        });
+    });
+  }
 
-    //collision with himself
-    for(let i = 0; i < snakeParts.length; i++){
-        let part = snakeParts[i];
-        if (part.x === headX && part.y === headY){
-            gameOver = true;
-            break;
-        }
-    }
-
-    if (gameOver) {
-        document.body.innerHTML = `<h1>game over</h1> <h1> Hai Totalizzato: </h1> <div id="score"> ${score} + <span>  Points </span></div>`;
-    }
-
-
-    return gameOver;
-}
+  return gameOver;
+};
 
 let clearScreen = () => {
-    ctx.fillStyle = "black"
-    ctx.fillRect(0,0, canvas.clientWidth, canvas.clientHeight);
-}
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+};
 
 let drawScore = () => {
-    scoreHTML.innerHTML= score + "<span> Points </span>";  
-}
-
+  scoreHTML.innerHTML = score + "<span> Points </span>";
+};
 
 let drawSnake = () => {
-    ctx.fillStyle = 'orange'
-    ctx.fillRect(headX * tileCount, headY * tileCount, tileSize, tileSize)
+  ctx.fillStyle = "orange";
+  ctx.fillRect(headX * tileCount, headY * tileCount, tileSize, tileSize);
 
-    ctx.fillStyle = 'green';
-    for (let i = 0; i < snakeParts.length; i++) {
-        let part = snakeParts[i];
-        ctx.fillRect(part.x * tileCount, part.y * tileCount, tileSize, tileSize)
-    }
+  ctx.fillStyle = "green";
+  for (let i = 0; i < snakeParts.length; i++) {
+    let part = snakeParts[i];
+    ctx.fillRect(part.x * tileCount, part.y * tileCount, tileSize, tileSize);
+  }
 
-    snakeParts.push(new SnakePart(headX, headY)); 
-    while(snakeParts.length > tailLength){
-        snakeParts.shift(); 
-    }
-
-
-}
+  snakeParts.push(new SnakePart(headX, headY));
+  while (snakeParts.length > tailLength) {
+    snakeParts.shift();
+  }
+};
 
 let changeSnakePosition = () => {
-    if (keyState[38] && yVelocity !== 1) { // Arrow Up
-        yVelocity = -1;
-        xVelocity = 0;
-    } else if (keyState[40] && yVelocity !== -1) { // Arrow Down
-        yVelocity = 1;
-        xVelocity = 0;
-    } else if (keyState[37] && xVelocity !== 1) { // Arrow Left
-        yVelocity = 0;
-        xVelocity = -1;
-    } else if (keyState[39] && xVelocity !== -1) { // Arrow Right
-        yVelocity = 0;
-        xVelocity = 1;
-    }
+  if (keyState[38] && yVelocity !== 1) {
+    // Arrow Up
+    yVelocity = -1;
+    xVelocity = 0;
+  } else if (keyState[40] && yVelocity !== -1) {
+    // Arrow Down
+    yVelocity = 1;
+    xVelocity = 0;
+  } else if (keyState[37] && xVelocity !== 1) {
+    // Arrow Left
+    yVelocity = 0;
+    xVelocity = -1;
+  } else if (keyState[39] && xVelocity !== -1) {
+    // Arrow Right
+    yVelocity = 0;
+    xVelocity = 1;
+  }
 
-    headX += xVelocity;
-    headY += yVelocity;
+  headX += xVelocity;
+  headY += yVelocity;
 };
 
 let drawFruit = () => {
-    ctx.fillStyle = 'red'
-    ctx.fillRect(fruitX * tileCount, fruitY * tileCount, tileSize, tileSize)
-}
+  ctx.fillStyle = "red";
+  ctx.fillRect(fruitX * tileCount, fruitY * tileCount, tileSize, tileSize);
+};
 
 //FRUTTO SPECIAL CHE DA 5 PUNTI!
 // let drawFruitSP = () => {
@@ -162,35 +199,31 @@ let drawFruit = () => {
 //     ctx.fillRect(fruitSPX * tileCount, fruitSPY * tileCount, tileSize, tileSize)
 // }
 
-
-
- let checkFruitCollision = () => {
-     if(fruitX == headX && fruitY == headY){
-        fruitX = Math.floor(Math.random() * tileCount);
-        fruitY = Math.floor(Math.random() * tileCount);
-        tailLength++;
-        score++;
-     }
- }
-
+let checkFruitCollision = () => {
+  if (fruitX == headX && fruitY == headY) {
+    fruitX = Math.floor(Math.random() * tileCount);
+    fruitY = Math.floor(Math.random() * tileCount);
+    tailLength++;
+    score++;
+  }
+};
 
 // keys trigger
 
-const keyState = {}; 
+const keyState = {};
 
 let keyDown = (event) => {
-    keyState[event.keyCode] = true;
+  keyState[event.keyCode] = true;
 };
 
 let keyUp = (event) => {
-    keyState[event.keyCode] = false;
+  keyState[event.keyCode] = false;
 };
 
 document.body.addEventListener("keydown", keyDown);
 document.body.addEventListener("keyup", keyUp);
 
-drawGame() 
-
+drawGame();
 
 //request Animation Frame
 
